1b642e5c7d73905134226a04935841fd6d75441c
neon520 <javierpg520@gmail.com>
2016-02-09 12:17:45 +0100

    cambiando redirecciones

--- rango/views.py
+++ rango/views.py
@@ -191,7 +191,7 @@ def user_login(request):
 				# If the account is valid and active, we can log the user in.
 				# We'll send the user back to the homepage.
 				login(request, user)
-				return HttpResponseRedirect('/rango/')
+				return HttpResponseRedirect('/')
 			else:
 				# An inactive account was used - no logging in!
 				return HttpResponse("Your Rango account is disabled.")
@@ -215,7 +215,7 @@ def user_logout(request):
 	logout(request)
 
 	# Take the user back to the homepage.
-	return HttpResponseRedirect('/rango/')
+	return HttpResponseRedirect('/')
 
 
 
